4f243377faa9e1e7a8f937686e8c8f81402f4919
wiltonabreu <wiltonabreuu@gmail.com>
2018-10-03 17:28:28 -0300

    Create users

--- index.php
+++ index.php
@@ -69,6 +69,71 @@ $app->get('/admin/logout', function() {
 	
 });
 
+$app->get('/admin/users', function() {
+    User::verifyLogin();
+
+   	$users = User::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users", array(
+		"users"=>$users
+	));
+	
+});
+
+$app->get('/admin/users/create', function() {
+    User::verifyLogin();
+    
+	$page = new PageAdmin();
+
+	$page->setTpl("users-create");
+	
+});
+
+$app->get('/admin/users/:iduser/delete', function($iduser) {
+    
+    User::verifyLogin();
+
+	
+});
+
+
+$app->get('/admin/users/:iduser', function($iduser) {
+    User::verifyLogin();
+    
+	$page = new PageAdmin();
+
+	$page->setTpl("users-update");
+	
+});
+
+
+$app->post('/admin/users/create', function() {
+    
+    User::verifyLogin();
+
+    $user = new User();
+
+    $_POST["inadimin"] = (isset($_POST["inadmin"]))?1:0;
+
+    $user->setData($_POST);
+
+	$user->save();
+
+	header("Location: /admin/users");
+	exit;
+
+
+});
+
+$app->post('/admin/users/:iduser', function($iduser) {
+    
+    User::verifyLogin();
+
+	
+});
+
 
 $app->run();
 
